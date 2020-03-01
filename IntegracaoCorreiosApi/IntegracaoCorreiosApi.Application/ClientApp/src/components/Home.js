5e1d1ffffeb347ba977b549fdd11e295f9b3837b
Renato Henrique Silva <re_hsilva@hotmail.com>
2020-03-01 01:22:54 -0300

    Merge branch 'master' of https://github.com/renatohsilva/IntegracaoCorreiosApi

--- IntegracaoCorreiosApi/IntegracaoCorreiosApi.Application/ClientApp/src/components/Home.js
+++ IntegracaoCorreiosApi/IntegracaoCorreiosApi.Application/ClientApp/src/components/Home.js
@@ -5,21 +5,13 @@ export class Home extends Component {
 
   render () {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
+        <div>
+            <label>Projeto feito a título de estudo, demonstrando como é possível fazer a integração com os Correios.</label>
+            <label>O web service utilizado na integração é o web service do SIGEP. Este web service possui diversos métodos.</label>
+            <label>O que é demonstrado neste exemplo é o método que retorna o endereço correspondente a um CEP.</label>
+            <label>O endereço do web service do SIGEP é o seguinte:</label>
+            https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl
+        </div>
     );
   }
 }
